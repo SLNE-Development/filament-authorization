2184aa6b7fe84c7fa0b1c2e21f2ae01db7197a0b
Ammo <ammo@slne.dev>
2026-02-12 17:54:36 +0100

    Bump version to 1.0.35 in composer.json and update FilamentAuthenticate to use getCurrentOrDefaultPanel

--- src/Http/Middleware/FilamentAuthenticate.php
+++ src/Http/Middleware/FilamentAuthenticate.php
@@ -2,26 +2,35 @@
 
 namespace SLNE\FilamentAuthorization\Http\Middleware;
 
+use Filament\Exceptions\NoDefaultPanelSetException;
 use Filament\Facades\Filament;
 use Filament\Models\Contracts\FilamentUser;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 use SLNE\FilamentAuthorization\FilamentAuthorizationPlugin;
 
 class FilamentAuthenticate extends Middleware
 {
+    /**
+     * @throws NoDefaultPanelSetException
+     * @throws AuthenticationException
+     */
     protected function authenticate($request, array $guards): void
     {
         $guard = Filament::auth();
 
         if (!$guard->check()) {
             $this->unauthenticated($request, $guards);
+
+            /** @noinspection PhpUnreachableStatementInspection */
+            return;
         }
 
         $this->auth->shouldUse(Filament::getAuthGuard());
 
         $user = $guard->user();
-        $panel = Filament::getCurrentPanel();
+        $panel = Filament::getCurrentOrDefaultPanel();
 
         if ($panel != null) {
             abort_if(
